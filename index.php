825a52084b3993c2cd15ac78ba0e0aac26661472
Sharuzzaman Ahmat Raslan <sharuzzaman@gmail.com>
2018-06-30 00:36:32 +1000

    add no cache
    add no cache

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+echo "<meta http-equiv=\"Cache-Control\" content=\"no-cache, no-store, must-revalidate\" />\n"
+echo "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n"
+echo "<meta http-equiv=\"Expires\" content=\"0\" />\n"
 echo "<html>";
 echo "Hello World";
 echo "<br>";
